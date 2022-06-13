92c9d72f8f299c919b8237f28fe089e502d7e721
Germinator <agbegermain@gmail.com>
2022-06-13 11:12:29 +0000

    refactor: Clear env content

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('.');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 if ($_POST) {

e7e05c1d92b31a5e7179725d207feb5594f538d3
Germinator <agbegermain@gmail.com>
2022-06-13 12:30:17 +0000

    refactor: Use heroku Mail To Go

--- contact.php
+++ contact.php
@@ -1,13 +1,9 @@
 <?php
 
+require("vendor/autoload.php");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
 if ($_POST) {
     $name = $_POST['name'];
@@ -20,25 +16,25 @@ if ($_POST) {
     try {
         $content = wordwrap(str_replace("\n.", "\n..", $message),70);
 
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
         $mail->isSMTP();
-        $mail->Host = $_ENV['MAIL_HOST'];
         $mail->SMTPAuth = true;
-        $mail->Username = $_ENV['MAIL_USERNAME'];
-        $mail->Password = $_ENV['MAIL_PASSWORD'];
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
 
-        $mail->setFrom($_ENV['MAIL_USERNAME']);
-        $mail->addAddress($_ENV['MAIL_ADDRESS'], $_ENV['MAIL_NAME']);
+        $mail->Host = getenv("MAILERTOGO_SMTP_HOST", true);
+        $mail->Port = intval(getenv("MAILERTOGO_SMTP_PORT", true));
+        $mail->Username = getenv("MAILERTOGO_SMTP_USER", true);
+        $mail->Password = getenv("MAILERTOGO_SMTP_PASSWORD", true);
+
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+        $mail->setFrom(getenv("MAILERTOGO_SMTP_USER", true));
+        $mail->addAddress(getenv("MAILERTOGO_DOMAIN", true), getenv("MAILERTOGO_NAME", true));
         $mail->addReplyTo($from, $name);
 
         $mail->isHTML(true);
         $mail->Subject = $subject;
         $mail->Body = $content;
         $mail->AltBody = $content;
-
-        echo json_encode($mail) . "\n";
 
         $mail->send();
     }

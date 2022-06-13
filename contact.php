ad98c7e713b577d5a0ed7dba4730f49077cd3bc6
Germinator <agbegermain@gmail.com>
2022-06-13 11:06:28 +0000

    refactor: Change env directory

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable('.');
 $dotenv->load();
 
 if ($_POST) {
